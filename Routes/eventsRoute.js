75cdcc014bc07804e46169653f10e32d8529344b
Sanath91009 <sanathprime@gmail.com>
2022-12-15 15:39:09 +0530

    added events

---
+++ Routes/eventsRoute.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const { getEvents } = require("../DB/models/Events");
+
+const eventsRouter = Router();
+
+eventsRouter.get("/getEvents", getEvents);
+
+module.exports = eventsRouter;
